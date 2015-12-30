0c8336addc5d409098ab965c0ffb96c2bc38918b
Fedor Rusak <fedor.rusak@gmail.com>
2015-12-30 16:06:09 +0300

    fix

--- win32.bat
+++ win32.bat
@@ -1,4 +1,6 @@
-if NOT %variables_were_set%==true (
+if EXIST release\win32\main.exe del release\win32\main.exe
+
+if NOT "%variables_were_set%"=="true" (
 
 	CALL "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\vsvars32.bat"
 	SET variables_were_set=true
@@ -7,4 +9,4 @@ if NOT %variables_were_set%==true (
 
 cl src/main.cpp src/engine.cpp src/glfw_wrapper.cpp src/openal_wrapper.cpp src/jx_wrapper.cpp src/opengl_wrapper.cpp src/freetype_wrapper.cpp  /Iinclude /IincludeGLM /Fo./obj/ /link /libpath:lib/win32 jx.lib  glfw3.lib User32.lib OpenGL32.lib  glew32.lib Gdi32.Lib shell32.lib OpenAL32.lib  alut.lib freetype.lib /out:release/win32/main.exe 
 
-release\win32\main.exe
+if EXIST release\win32\main.exe CALL release\win32\main.exe
