7e1b503a227351a71fc2b10e490e76a4fc2a1c5a
Fedor Rusak <fedor.rusak@gmail.com>
2017-05-19 12:12:18 +0300

    Now we can close window!

--- rusty/hello.rs
+++ rusty/hello.rs
@@ -16,7 +16,9 @@ pub enum GLFWwindow {}
 #[link(name = "glfw3")]
 extern {
 	fn glfwInit() -> c_int;
+	fn glfwPollEvents() -> c_int;
 	fn glfwCreateWindow(width: c_int, height: c_int, title: *const c_char, monitor: *mut GLFWmonitor, share: *mut GLFWwindow) -> *mut GLFWwindow;
+	fn glfwWindowShouldClose(window: *mut GLFWwindow) -> c_int;
 }
 
 fn main() {
@@ -26,7 +28,19 @@ fn main() {
 
 		glfwInit();
 
-		glfwCreateWindow(800 as c_int, 600 as c_int, title, ptr::null_mut(), ptr::null_mut())
+		let window = glfwCreateWindow(800 as c_int, 600 as c_int, title, ptr::null_mut(), ptr::null_mut());
+
+
+		println!("WORKING");
+
+		loop {
+			glfwPollEvents();
+
+			if glfwWindowShouldClose(window) != 0 {
+				break;
+			}
+		};
+
+		println!("ENDED");
 	};
-	print!("WORKING"); loop{};
 }
